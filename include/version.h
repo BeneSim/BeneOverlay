974447040e3ae97f34a071db31c6f1585050646d
Benjamin Isbarn <babene88@gmail.com>
2017-06-18 11:47:59 +0200

    Corrected version number

--- include/version.h
+++ include/version.h
@@ -2,9 +2,9 @@
 #define VERSION_H
 
 int const VERSION_MAJOR = 1;
-int const VERSION_MINOR = 0;
+int const VERSION_MINOR = 1;
 int const VERSION_PATCH = 0;
-bool const VERSION_PRERELEASE = false;
+bool const VERSION_PRERELEASE = true;
 
 QString const VERSION_STRING = VERSION_PRERELEASE ? QString("%1.%2.%3 (Beta)").arg(VERSION_MAJOR).arg(VERSION_MINOR).arg(VERSION_PATCH) : QString("%1.%2.%3").arg(VERSION_MAJOR).arg(VERSION_MINOR).arg(VERSION_PATCH);
 
