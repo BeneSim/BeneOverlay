ff6acd3d8cdafe793a9c39edd77dc9832b8e00de
Benjamin Isbarn <babene88@gmail.com>
2017-07-04 15:06:28 +0200

    Changed version number to stable

--- include/version.h
+++ include/version.h
@@ -4,7 +4,7 @@
 int const VERSION_MAJOR = 1;
 int const VERSION_MINOR = 2;
 int const VERSION_PATCH = 0;
-bool const VERSION_PRERELEASE = true;
+bool const VERSION_PRERELEASE = false;
 
 QString const VERSION_STRING = VERSION_PRERELEASE ? QString("%1.%2.%3 (Beta)").arg(VERSION_MAJOR).arg(VERSION_MINOR).arg(VERSION_PATCH) : QString("%1.%2.%3").arg(VERSION_MAJOR).arg(VERSION_MINOR).arg(VERSION_PATCH);
 
